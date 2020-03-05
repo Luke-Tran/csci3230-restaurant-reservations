4409d0b22946699fb6b6f5b911da77ea85b9833c
Luke-Tran <luketran08@gmail.com>
2020-03-05 13:45:21 -0500

    Worked on formatting the layout of the grid

--- scripts/menu.js
+++ scripts/menu.js
@@ -10,6 +10,10 @@ $(document).ready(function() {
 });
 
 function addCategory(category) {
-  console.log(category.categoryName);
-  $("#categories").append(`<tr><td>${category.categoryName}</td></tr>`);
+  var categoryImg = $(`<div><img src="${category.imageURL}" alt="${category.imageAlt}" height="42" width="42"></div>`);
+  var categoryBtn = $(`<div><button class="categoryBtn">${category.categoryName}</button></div>`);
+  var row = $("<tr></tr>");
+  var content = $("<div></div>").append(categoryImg, categoryBtn);
+  row.append($("<td></td>").append(content));
+  $("#categories").append(row);
 }
