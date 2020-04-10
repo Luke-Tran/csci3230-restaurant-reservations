d6e869a555494e9f4598b3886ac80431c0768e72
Gajan Sivanesan <Gajan.Sivanesan@ontariotechu.net>
2020-04-10 16:12:48 -0400

    modified index file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+let message2 = "Hello from server";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -35,9 +38,51 @@ app.get('/menu', (request, response) => {
 app.set('port', 3000);
 
 // Show which port the server is lisening at 
-app.listen(app.get('port'), () => {
-	console.log("Server is listening at port: " + app.get('port'));
+// app.listen(app.get('port'), () => {
+// 	console.log("Server is listening at port: " + app.get('port'));
+// });
+
+// Set up socket IO
+io.on('connection', function(socket){
+	console.log('User Connected');
+
+	// Accepts what the client is sending the server
+	socket.on('send message', function(data){
+		console.log(data);
+	});
+
+	// Executes the function once after x numnber of miliseconds (currently 10 seconds)
+	var data = {serverMessage: message2,
+					serverTime: serverTime(),
+	};
+	setTimeout(function() {io.emit('send message', data)},5000);
+	//setTimeout(function() {io.emit('send message',"time" )},1000);
+	//io.emit('send message', message);
+
 });
+
+// Show which port the server is lisening at 
+//app.listen(app.get('port'), () => {
+	//console.log("Server is listening at port: " + app.get('port'));
+//});
+http.listen(app.get('port'), function() {
+    console.log('Listening on port ' + app.get('port'));
+});
+
+// Function to get the server time when entering a message
+function serverTime() {
+	var d = new Date();
+	var hours = d.getHours();
+	var minutes = d.getMinutes();
+	// Format the minutes so they give a leading 0 if less than 10
+	if (minutes < 10) {
+		var time = `${hours}:0${minutes}`;
+	}
+	else {
+		var time = `${hours}:${minutes}`;
+	}
+	return time;
+}
 
 
 
